309d2ad9cc6c1462df4c75d7e44da8823c088d63
@ontheroad_jp <dev@ontheroad.jp>
2015-10-28 12:50:32 +0900

    initial

---
+++ src/partials/footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">Version1.0</div>
+    <strong>Copyright &copy; 2015 <a href="#">Company</a>.</strong> All rights reserved.
+</footer>
---
+++ src/partials/main_header_min.php
@@ -0,0 +1,41 @@
+<header class="main-header">
+
+<!-- Logo -->
+<a href="" class="logo">
+  <!-- mini logo for sidebar mini 50x50 pixels -->
+  <span class="logo-mini"><b>A</b>LT</span>
+  <!-- logo for regular state and mobile devices -->
+  <span class="logo-lg"><b>Admin</b>LTE</span>
+</a>
+
+<nav class="navbar navbar-static-top" role="navigation">
+
+    <!-- メニュー項目 -->
+    <!-- 小さくなった時に消す -->
+    <!-- <div class="collapse navbar-collapse" id="navbar-collapse"> -->
+
+        <!-- サイドバー制御 -->
+        <a href="" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <div class="navbar-custom-menu">
+
+            <ul class="nav navbar-nav">
+                <li><a href="">顧客管理</a></li>
+                <li><a href="">売上管理</a></li>
+                <!-- doropdown -->
+                <li class="dropdown">
+                    <a href="" class="dropdown-toggle" data-toggle="dropdown">その他<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="">その他１</a></li>
+                        <li><a href="">その他２</a></li>
+                    </ul>
+                </li>
+                <li><a href="auth/login_simplest.php"><i class="fa fa-sign-out"></i>ログアウト</a></li>
+            </ul>
+        </div>
+    <!-- </div> -->
+
+</nav>
+</header><!-- end header -->
